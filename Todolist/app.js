50ccf43f453cd46ee4c4a6dece4ec71e64c32452
Kelly online <kellyonline2016@gmail.com>
2020-04-13 22:49:08 +0100

    create a todo to dom

--- Todolist/app.js
+++ Todolist/app.js
@@ -18,6 +18,18 @@ function addTodo(event) {
     //PREVENT FORM FROM SUBMITTING BY DEFAULT
 
     event.preventDefault();
+
+    //Create todo div
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    
+   
     console.log("Hello bro!");
     
 }
+<div class="todo">
+<li></li>
+<button>Delete</button>
+<button>Checked</button>
+</div>
