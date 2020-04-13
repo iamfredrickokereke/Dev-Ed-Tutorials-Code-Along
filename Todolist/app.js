c6c044306e3080c891eb071ec402531f026cc403
Kelly online <kellyonline2016@gmail.com>
2020-04-13 21:58:59 +0100

    fix class selector

--- Todolist/app.js
+++ Todolist/app.js
@@ -2,8 +2,8 @@
 // Selectors
 
 const todoInput = document.querySelector('.todo-input')
-const todoButton = document.querySelector('.todo-input')
-const todoList = document.querySelector('.todo-input')
+const todoButton = document.querySelector('.todo-button')
+const todoList = document.querySelector('.todo-list')
 
 
 // Event Listeners
