a4221977e089971e2b52785ff19d21f0afec7525
Kelly online <kellyonline2016@gmail.com>
2020-04-13 23:16:52 +0100

    create a check mark button and append to new tododiv

--- Todolist/app.js
+++ Todolist/app.js
@@ -20,25 +20,26 @@ function addTodo(event) {
     event.preventDefault();
 
     //Create todo div
-
     const todoDiv = document.createElement('div');
-    todoDiv.classList.add("todo");
-    
+    todoDiv.classList.add("todo");    
      // create Li
-
      const newTodo = document.createElement('li');
      newTodo.innerText = 'hey';
-
      newTodo.classList.add('todo-item');
-
-
      // append to div
-
      todoDiv.appendChild(newTodo)
 
+     //CREATE CHECK MARK BUTTON
+
+     const completedButton = document.createElement('button');
+     completedButton.innerHTML = ' <i class="fas fa-check"></i>';
+     completedButton.classList.add('complete-btn')
+
+     todoDiv.appendChild(completedButton);
 
 
-    console.log("Hello bro!");
+
+    console.log(todoDiv);
     
 }
 
